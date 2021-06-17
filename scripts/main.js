03dab1e6a7055679e0544d7f1e242267ffa0a184
OSAndrew <jackthecat70@gmail.com>
2021-06-17 15:45:37 -0400

    Added functionality for links to open in a new tab. Changed title of index page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,24 @@
-let displayImage = document.querySelector('img')
+// Ensure all links will open in a new tab
+document.querySelector('a').setAttribute('target', '_blank');
+document.querySelector('a').setAttribute('rel', 'noopener noreferrer');
+
+let displayImage = document.querySelector('img');
 
 displayImage.onclick = function() {
     let displayImageSrc = displayImage.getAttribute('src');
     if(displayImageSrc === 'images/burial.jpg')
-        displayImage.setAttribute('src', 'images/burial-face.jpg')
+        displayImage.setAttribute('src', 'images/burial-face.jpg');
     else
-        displayImage.setAttribute('src', 'images/burial.jpg')
+        displayImage.setAttribute('src', 'images/burial.jpg');
 }
 
-let supriseButton = document.querySelector('button')
-let myHeader = document.querySelector('h1')
+let supriseButton = document.querySelector('button');
+let myHeader = document.querySelector('h1');
 
 function changeHeader() {
-    myHeader.textContent = 'UNTRUE!'
+    myHeader.textContent = 'UNTRUE!';
 }
 
 supriseButton.onclick = function() {
-    changeHeader()
+    changeHeader();
 }
